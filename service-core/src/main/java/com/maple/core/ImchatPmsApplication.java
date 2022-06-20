bc1180a08861e937605baacab3b56492f66a1366
高冠祺 <307590625@qq.com>
2022-06-20 21:07:47 +0800

    init

---
+++ service-core/src/main/java/com/maple/core/ImchatPmsApplication.java
@@ -0,0 +1,13 @@
+package com.maple.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImchatPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImchatPmsApplication.class, args);
+    }
+
+}
